5df5e3749678ddb6dc3b842b36446e7c5e962bb8
FRANCISCO <francisco_nascimento@outlook.com.br>
2016-09-17 16:45:15 -0300

    Foi inserido o controle de fluxo "pass" nos métodos (que estão vazios) para que possam "interpretar"

--- BlocoDeNotas.py
+++ BlocoDeNotas.py
@@ -5,12 +5,17 @@ print("Classe BlocoDeNotas")
 class blocoDeNotas:
     notas = []
     def __init__(self):
+        pass
         
     def buscar(self):
+        pass
         
     def nova_nota(self):
+        pass
     
     def alterar_texto(self):
+        pass
         
     def alterar_titulo(self):
+        pass
         
