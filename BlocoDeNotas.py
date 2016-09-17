1274c0071a0ef80ef3605212016a6d51f71c6f39
FRANCISCO <francisco_nascimento@outlook.com.br>
2016-09-17 16:39:50 -0300

    Foi criado o modulo BlocoDeNotas que contem a classe blocoDeNotas, nesta classe foi definido os métodos buscar, nova_nota, alterar_texto e alterar_titulo.

---
+++ BlocoDeNotas.py
@@ -0,0 +1,16 @@
+print("Classe BlocoDeNotas")
+
+
+
+class blocoDeNotas:
+    notas = []
+    def __init__(self):
+        
+    def buscar(self):
+        
+    def nova_nota(self):
+    
+    def alterar_texto(self):
+        
+    def alterar_titulo(self):
+        
