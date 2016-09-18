704f88569c3ba18357f8f27ef906476ce361915c
FRANCISCO <francisco_nascimento@outlook.com.br>
2016-09-18 12:06:11 -0300

    Foi modificado o método "adicionar_notas" e "imprimir_notas", o método "imprimir_notas" foi movido para o modulo "BlocoDeNotas"

--- BlocoDeNotas.py
+++ BlocoDeNotas.py
@@ -1,9 +1,11 @@
 print("Classe BlocoDeNotas")
 
 from Notas import Notas
+import random
 
 class BlocoDeNotas:
     notas = []
+    indice = 0
     def __init__(self):
         self.notasObj = Notas()
                 
@@ -11,8 +13,31 @@ class BlocoDeNotas:
         pass
         
     def adicionar_notas(self):
+        self.__class__.indice += 1
         texto = input("Digite o texto da nota: ")
         self.notasObj.setTexto(texto)
+        titulo = input("Digite o titulo da nota: ")
+        self.notasObj.setTitulo(titulo)
+        
+        #for i in range(10):
+        #    id= random.random()
+            
+            
+        def gera_random():
+            return random.randrange(10)            
+            
+        id = gera_random()    
+            
+                   
+        self.notasObj.setId(id)
+        
+        self.__class__.notas.append(self)
+        
+    def imprimir_notas(self, nota=None):
+        if not nota:
+            nota = self.notas #Deve ter uma lista para as notas
+        for n in nota:
+            print("{0}: {1}\n{2}".format(self.indice,n.notasObj.titulo,n.notasObj.id))
     
     def alterar_texto(self):
         pass
--- Menu.py
+++ Menu.py
@@ -7,7 +7,7 @@ class Menu:
     def __init__(self):
         self.blocodenotas = BlocoDeNotas()
         self.escolhas = {
-        "1": self.imprimir_notas,
+        "1": self.blocodenotas.imprimir_notas,
         "2": self.buscar_notas,
         "3": self.blocodenotas.adicionar_notas,
         "4": self.modificar_notas,
@@ -26,20 +26,16 @@ class Menu:
 
     def executar(self): #É executado eternamente apresentado o menu com as escolhas para selecionar uma opção
         while True: #Verdadeiro pra sempre
-            self.apresentar_menu #mostra o metodo apresentar menu
+            self.apresentar_menu() #mostra o metodo apresentar menu
             escolha = input("escolha uma opcao: ") #Tipo um scanf no C; Antigo raw_input
             acao = self.escolhas.get(str(escolha)) #Get=Busca no dicionario, é um metodo do python
             if acao:
                 acao()
             else:
                 print ("{0} nao foi uma escolha valida.".format(escolha))
-                
-    def imprimir_notas(self, nota=None):
-        if not nota:
-            nota = self.blocodenotas.notas #Deve ter uma lista para as notas
-        for n in nota:
-            print("{0}: {1}\n{2}".format(n.id,n.titulo, n.texto))
-        
+              
+    
+            
     def buscar_notas(self):
         filtro = input("Digite o ID da nota: ")
         nota = self.blocodenotas.buscar(filtro)
